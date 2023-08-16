1788f266bacb17c269dbab4582a141b705ee2339
DannielNavas <dannielnavas@gmail.com>
2023-08-16 08:27:18 -0500

    ✨ data api integration

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -37,21 +37,33 @@ export default {
   },
   data() {
     return {
-      post: {
-        slug: 'mi-primer-post',
-        title: 'Mi primer post',
-        author: 'Diana Martínez',
-        updated: '8/06/2022',
-        description: 'Lorem ispum dolor sit amet',
-        cover: 'https://via.placeholder.com/1024x420',
-        content: '# Title\n\n## Second title\n\nLorem ipsum dolor sit amet',
-      },
+      // post: {
+      //   slug: 'mi-primer-post',
+      //   title: 'Mi primer post',
+      //   author: 'Diana Martínez',
+      //   updated: '8/06/2022',
+      //   description: 'Lorem ispum dolor sit amet',
+      //   cover: 'https://via.placeholder.com/1024x420',
+      //   content: '# Title\n\n## Second title\n\nLorem ipsum dolor sit amet',
+      // },
     }
   },
   head() {
     return {
       title: this.post?.title,
       meta: [{ name: 'description', content: this.post?.description || '' }],
+    }
+  },
+  computed: {
+    post() {
+      return {
+        title: this.article?.title,
+        author: this.article['author-name'][0],
+        updated: new Date(this.article?.updated).toLocaleDateString(),
+        description: this.article?.description,
+        cover: this.article?.cover[0].thumbnails.full.url,
+        content: this.article?.content,
+      }
     }
   },
 }
